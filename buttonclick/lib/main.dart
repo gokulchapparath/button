3dee01622364f6d454432afba32e872b9425177a
Gokul <gokul@Foxfennecss-iMac.local>
2019-07-15 13:49:51 +0530

    add

---
+++ buttonclick/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'Buttonpage.dart';
+void main()=>runApp(Btnclk());
+class Btnclk extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home:Buttonpage(),
+    );
+  }
+}
