c31147701c238d7e4d67564d179460a32e5e1e69
gokulchapparath <gokulchapparath@gmail.com>
2019-07-16 17:05:32 +0530

    click

--- buttonclick/lib/Full.dart
+++ buttonclick/lib/Full.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart' as prefix0;
 import 'dart:async';
 import 'main.dart';
 import 'Buttonpage.dart';
+import 'Score.dart';
+
 class Full extends StatefulWidget {
   @override
   _FullState createState() => _FullState();
 }
 
 class _FullState extends State<Full> {
-  
-  
   double countval = 0;
   int total = 0;
   double sizes = 0;
@@ -18,12 +18,22 @@ class _FullState extends State<Full> {
   double countval1 = 0;
   int total1 = 0;
   double sizes1 = 0;
+
+
   @override
   Widget build(BuildContext context) {
-    Timer.periodic(Duration(seconds: 60), (timer) {
-      _Disps(context,countval,countval1);
-       Navigator.of(context).pop(context);
-});
+    Timer.periodic(Duration(seconds: 30), (timer) {
+      // _Disps(context,countval,countval1);
+      //  Navigator.of(context).pop(context);
+deactivate();
+dispose();
+      timer.cancel();
+      var route = new MaterialPageRoute(
+          builder: (BuildContext context) => Score(
+              p1: (countval1.toInt()).toString(),
+              p2: (countval.toInt()).toString()));
+      Navigator.of(context).push(route);
+    });
     if (countval >= 200) {
       sizes = 200;
     }
@@ -32,8 +42,8 @@ class _FullState extends State<Full> {
       sizes1 = 200;
     }
     String my1 = (countval1.toInt()).toString();
+
     return Scaffold(
-      
       body: Column(
         children: <Widget>[
           Expanded(
@@ -42,9 +52,11 @@ class _FullState extends State<Full> {
               child: MaterialButton(
                 color: Colors.white,
                 onPressed: () {
-                  setState(() => total += 1);
-                  setState(() => countval += 1);
-                  setState(() => sizes += 1);
+                  setState(() {
+                    total++;
+                    countval++;
+                    sizes++;
+                  });
                 },
                 splashColor: Colors.black,
 
@@ -61,9 +73,11 @@ class _FullState extends State<Full> {
             child: MaterialButton(
               color: Colors.black,
               onPressed: () {
-                setState(() => total1 += 1);
-                setState(() => countval1 += 1);
-                setState(() => sizes1 += 1);
+                setState(() {
+                  total1++;
+                  countval1++;
+                  sizes1++;
+                });
               },
               splashColor: Colors.white,
               child: Text(
@@ -76,36 +90,33 @@ class _FullState extends State<Full> {
         ],
       ),
     );
-
-    
   }
 }
 
+// void _Disps(context,countval,countval1)
+//     {
+//       showDialog(
+//         context: context,
+//         builder: (BuildContext context)
+//         {
+//               return AlertDialog(
+//                   title: new Text("Scores"),
+//                 content:new Text("times up !!\nplayer 1:"+(countval1.toInt()).toString()+"\nplayer 2:"+(countval.toInt()).toString()),
+//                 actions: <Widget>[
+//                   new FlatButton(
+//                     child: new Text("NEW GAME"),
+//                     onPressed: (){
+//                       // Navigator.of(context).pop(context);
+//                       Navigator.push(
+//         context,
+//         new MaterialPageRoute(builder: (context) => new Full()),
 
-void _Disps(context,countval,countval1)
-    {
-      showDialog(
-        context: context,
-        builder: (BuildContext context)
-        {
-              return AlertDialog(
-                  title: new Text("Scores"),
-                content:new Text("times up !!\nplayer 1:"+(countval1.toInt()).toString()+"\nplayer 2:"+(countval.toInt()).toString()),
-                actions: <Widget>[
-                  new FlatButton(
-                    child: new Text("close"),
-                    onPressed: (){
-                      Navigator.of(context).pop(context);
-                      Navigator.push(
-        context,
-        new MaterialPageRoute(builder: (context) => new Buttonpage()),
-        
-      );
-                    },
-                  )
-                ],
-                 
-              );
-        }
-      );
-    }
+//       );
+//                     },
+//                   )
+//                 ],
+
+//               );
+//         }
+//       );
+//     }
